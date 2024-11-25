8c4566456067ffd464c7e9ded764fe6e41a43dc5
Valentinvallejo5 <vvallejo@udesa.edu.ar>
2024-11-25 17:19:17 -0300

    js home

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-let container1= document.querySelector("recetas")
-let url='https://dummyjson.com/recipes'
+let container1 = document.querySelector(".recetas"); // Selecciona correctamente el contenedor con clase "recetas"
+let url = "https://dummyjson.com/recipes";
 
 fetch(url)
     .then(function (resp) {
-    return resp.json();
+        return resp.json(); // Convierte la respuesta a JSON
     })
     .then(function (data) {
-    console.log(data);
+        console.log(data); // Muestra todos los datos en la consola
+        let recipes = data.recipes; // Accede al array de recetas
+
+        // Itera sobre las primeras 10 recetas
+        for (let i = 0; i < 10; i++) {
+            container1.innerHTML += `
+                <article class="imagenes">
+                    <p class="fee"><i> <b>${recipes[i].name}<b/> </i></p>
+                    <img src= "https://cdn.dummyjson.com/recipe-images/${recipes[i].id}.webp"class="receta-img">
+                    <p>Ingredientes: ${recipes[i].ingredients.join(", ")}</p>
+                </article>`;
+        }
+    })
+    .catch(function (error) {
+        console.error("Error al obtener las recetas:", error);
     });
+
 
 

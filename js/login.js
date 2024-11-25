daafb727551beae021dc37682eafc59c64f70c2c
Matefascetto <mfascetto@udesa.edu.ar>
2024-11-25 18:49:17 -0300

    agregue registro js

--- js/login.js
+++ js/login.js
@@ -3,17 +3,18 @@ let form = document.querySelector(".login");
 let email = document.querySelector(".caja-email");
 let contraseña = document.querySelector(".caja-contraseña");
 
-let noemail = document.querySelector(".invalid-feedback.email");
-let nocontra = document.querySelector(".invalid-feedback.contraseña");
+let noemail = document.querySelector(".invalido");
+let nocontra = document.querySelector(".invalida");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     let verdad = true;
 
-    
+   
     if (email.value == "") {
         noemail.style.display = "block";
-        noemail.innerText = "Por favor complete el campo email.";
+        noemail.innerText = "Por favor complete el campo email";
+        alert("Por favor complete el campo email");
         verdad = false;
     } else {
         noemail.style.display = "none"; 
@@ -22,7 +23,8 @@ form.addEventListener("submit", function (e) {
    
     if (contraseña.value == "") {
         nocontra.style.display = "block";
-        nocontra.innerText = "Por favor complete el campo contraseña.";
+        nocontra.innerText = "Por favor complete el campo contraseña";
+        alert("Por favor complete el campo contraseña");
         verdad = false;
     } else {
         nocontra.style.display = "none"; 
